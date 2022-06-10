89027dbab6c703e91576faa64e82357dc9467c4f
jehala <jenniferehala@github.com>
2022-06-10 08:54:58 -0400

    commit

--- Algorithims/uncompress.js
+++ Algorithims/uncompress.js
@@ -1,29 +1,61 @@
-const uncompress = (s) => {
-    var res = [];
-    var i = 0;
-    for (var j = 1; j < s.length; j++) {
-        const isNum = !isNaN(s[j])
-        if (isNum) {
-            j++;
-        }
-        const num = Number(s.slice(i, j));
-        for (let count = 0; count < num; count++) {
-            if (s[j] > 0) {
-                break;
-            }
-            res.push(s[j]);
+// const uncompress = (s) => {
+//     var res = [];
+//     var i = 0;
+//     for (var j = 1; j < s.length; j++) {
+//         const isNum = !isNaN(s[j])
+//         if (isNum) {
+//             j++;
+//         }
+//         const num = Number(s.slice(i, j));
+//         for (let count = 0; count < num; count++) {
+//             if (s[j] > 0) {
+//                 break;
+//             }
+//             res.push(s[j]);
 
+//         }
+//         j++
+//         i = j
+//     }
+//     return res.join("");
+// };
+
+// console.log(uncompress("3n12e2z")) // 'nnneeeeeeeeeeeezz'
+// console.log(uncompress("2c3a1t")) // "ccaaat"
+// console.log(uncompress("4s2b")) // 'ssssbb'
+// console.log(uncompress("2p1o5p")) // 'ppoppppp'
+// console.log(uncompress("10y")) // 'yyyyyyyyyyyyyyyyyyyyyyyyy'
+
+// Smaller Time Complexity
+
+const uncompress = (s) => {
+    var result = [];
+    var numbers = "0123456789";
+    var i = 0;
+    var j = 0;
+
+    while (j < s.length) {
+
+        if (numbers.includes(s[j])) {
+            j++
         }
-        j++
-        i = j
+
+        else {
+            var num = s.slice(i, j)
+            for (count = 0; count < num; count++) {
+                result.push(s[j])
+            }
+            j++
+            i = j
+        }
+
     }
-    return res.join("");
+
+    return result.join("");
 };
 
 console.log(uncompress("3n12e2z")) // 'nnneeeeeeeeeeeezz'
 console.log(uncompress("2c3a1t")) // "ccaaat"
 console.log(uncompress("4s2b")) // 'ssssbb'
 console.log(uncompress("2p1o5p")) // 'ppoppppp'
-console.log(uncompress("10y")) // 'yyyyyyyyyyyyyyyyyyyyyyyyy'
-
-// Smaller Time Complexity
+console.log(uncompress("25y")) // 'yyyyyyyyyyyyyyyyyyyyyyyyy'
