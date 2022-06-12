63405b3d58b35fc7c71ed22d73b4b16bac799cc9
jehala <jenniferehala@github.com>
2022-06-12 09:28:41 -0400

    commit

--- Algorithims/anagram_soln.js
+++ Algorithims/anagram_soln.js
@@ -1,20 +1,36 @@
 const anagrams = (s1, s2) => {
-
     let count = {}
 
     for (let char in s1) {
-
+        // iterate through the keys
+        // then set key to 0
+        // if found, we increment count object key
         if (!(char in count)) {
-            //this is to initalize the character into count object
             count[char] = 0
         }
         count[char] += 1;
     }
-
-    for (var i = 0; i < s2.length; i++) {
-
+    // iterate over property values
+    // if not found, return false
+    // if found, decrement object key so a:1-1=0 => a:0, b:1, c:1
+    for (let char of s2) {
+        if (char === null) {
+            return false
+        }
+        else {
+            count[char] -= 1
+        }
     }
 
+    //still have to loop through object
+    // if the keys don't equal 0, then its false. All keys should equal zero.
+    for (let char in count) {
+        if (count[char] != 0) {
+            return false;
+        }
+    }
+
+    return true;
 
 };
 
